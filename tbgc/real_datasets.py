eed88159a05a3179fe1551073475affb21aef112
Mateus Riva <mateus.riva@telecom-paris.fr>
2020-04-01 10:29:42 +0200

    Prepare noisy real experiments

--- tbgc/real_datasets.py
+++ tbgc/real_datasets.py
@@ -7,13 +7,15 @@ import networkx as nx
 import wget
 
 
-def load_email_dataset(cut=None, sparse=False):
+def load_email_dataset(cut=None, noise=0):
     """Loads the email-Eu-core dataset, along with its base communities.
 
     Parameters
     ----------
     cut : None, int
-        Whether to cut the graph. If int, cuts neighbors above `cut`"""
+        Whether to cut the graph. If int, cuts neighbors above `cut`
+    noise : int
+        Std of mean-zero gaussian noise to add to model. Default: zero"""
     # Checking if present, downloading if not
     if not os.path.exists("data/email-Eu-core.txt.gz"):
         wget.download("https://snap.stanford.edu/data/email-Eu-core.txt.gz", out="data/email-Eu-core.txt.gz")
@@ -35,6 +37,10 @@ def load_email_dataset(cut=None, sparse=False):
     dict_of_labels = dict(list_of_tuples)
 
     A_M, A_O, vertex_labels = process_loaded_graph(graph_email, dict_of_labels, cut)
+
+    # Adding noise
+    if noise > 0:
+        A_M = A_M + np.random.normal(0, noise, A_M.shape)
 
     return None, A_M, None, None, A_O, None, vertex_labels
 
--- tbgc/real_experiment.py
+++ tbgc/real_experiment.py
@@ -11,18 +11,18 @@ from real_datasets import load_email_dataset, load_dbpl_dataset, load_custom_dbp
 
 def pooled_iteration_function(iteration):
     """Used for the multiprocessing Pool of run_iterations."""
-    return run_iteration(graph_function, (10000,), random_state=iteration)
+    return run_iteration(graph_function, (10000, noise), random_state=iteration)
 
 if __name__ == '__main__':
     # Experiment parameters
-    graphs = list(zip(["DBPL"], [load_custom_dbpl_dataset]))
+    graphs = list(zip(["email"], [load_email_dataset], [0,1,2,5,10,20,30]))
     range_of_iterations = range(40)
 
     # Experiment results dict
-    results = {}
+    results = {graph[0] : {} for graph in graphs}
 
-    for graph_name, graph_function in graphs:
-        print("Experiment on {}".format(graph_name))
+    for graph_name, graph_function, noise in graphs:
+        print("Experiment on {} with {} noise".format(graph_name, noise))
 
         experiment_measures = {"template_adj": {"ari": [], "projector_distance": [], "time": []},
                                "template_lap": {"ari": [], "projector_distance": [], "time": []},
@@ -40,7 +40,7 @@ if __name__ == '__main__':
                         experiment_measures[method][measure].append(iteration_measures[method][measure])
                         print(iteration_measures)
 
-        results[graph_name] = experiment_measures
+        results[graph_name][noise] = experiment_measures
 
-    with open("real_results2.json", "w") as fp:
+    with open("real_results_noisy.json", "w") as fp:
         json.dump(results, fp, indent=2)
