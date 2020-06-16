b4fd1b15bf4deddf4ff664114339e5afb5a0d3ab
Mateus Riva <mateus.riva@telecom-paris.fr>
2020-06-16 16:08:21 +0200

    update real experiment to include louvain

--- tbgc/real_experiment.py
+++ tbgc/real_experiment.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
         experiment_measures = {"template_adj": {"ari": [], "projector_distance": [], "time": []},
                                "template_sto": {"ari": [], "projector_distance": [], "time": []},
                                "spectral": {"ari": [], "projector_distance": [], "time": []},
+                               "modularity_louv": {"ari": [], "projector_distance": [], "time": []},
                                "modularity": {"ari": [], "projector_distance": [], "time": []}}
 
         with Pool(8) as pool:
@@ -35,7 +36,7 @@ if __name__ == '__main__':
             #    iteration_measures = run_iteration(graph_function, (c,), random_state=iteration)
 
             for iteration_measures in iteration_measures_list:
-                for method in ["template_adj", "template_sto", "spectral", "modularity"]:
+                for method in ["template_adj", "spectral", "modularity", "modularity_louv"]:
                     for measure in ["ari", "projector_distance", "time"]:
                         experiment_measures[method][measure].append(iteration_measures[method][measure])
                         print(iteration_measures)
